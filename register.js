a351d9b1c98eb9f068b21180f87ef47cd4d787e0
Darshan <darshanbpattar@gmail.com>
2022-06-14 18:29:54 +0530

    Added register page and modified login page

---
+++ register.js
@@ -0,0 +1,7 @@
+function Glow(){
+    document.getElementById('submitbtn').style.backgroundColor = "rgb(255, 10, 10)";
+}
+
+function Dim(){
+    document.getElementById('submitbtn').style.backgroundColor = "rgb(255, 51, 51)";
+}
